c1d2cbdb108221aec4f8f42bf0ae3e7df004712f
Nexus8th <Nexus8th@users.noreply.github.com>
2022-03-12 13:21:10 +0100

    Couples fix added

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,22 +1,33 @@
 const Todo = ({ setInputText, todos, setTodos, inputText }) => {
-    
-const inputTextField = (e) => {
-    setInputText(e.target.value)
-};
-const submitTodo = (e) => {
+  const inputTextField = (e) => {
+    setInputText(e.target.value);
+  };
+  const submitTodo = (e) => {
     e.preventDefault();
+    setTodos([
+      ...todos,
+      { text: inputText, id: Math.random() * 100000, completed: false },
+    ]);
     setInputText("");
-    setTodos([...todos, {text: inputText, id: Math.random() * 100000, completed: false }]);
-}
+  };
 
-    return (
-        <div className="todo-container">
-            <form className="form">
-                <input value={inputText} onChange={inputTextField} className="form-input-text" placeholder="Ajouter une tâche" type="text" required></input>
-                <button onClick={submitTodo} className="form-add-btn" type="submit">Ajouter</button>
-            </form>
-        </div>
-    );
+  return (
+    <div className="todo-container">
+      <form className="form">
+        <input
+          value={inputText}
+          onChange={inputTextField}
+          className="form-input-text"
+          placeholder="Ajouter une tâche"
+          type="text"
+          required
+        ></input>
+        <button onClick={submitTodo} className="form-add-btn" type="submit">
+          Ajouter
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Todo;
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,31 +1,38 @@
-const Todo = ({text, todo, todos, setTodos}) => {
-
-const deleteBtn = () => {
-    setTodos(todos.filter(el => el.id !== todo.id));
-}
-const editBtn = () => {
-    
-}
-const checkBtn = () => {
-    setTodos(todos.map(item => {
-        if(item.id === todo.id) {
-            return {
-                ...item, completed: !item.completed
-            }
+const Todo = ({ text, todo, todos, setTodos }) => {
+  const deleteBtn = () => {
+    setTodos(todos.filter((el) => el.id !== todo.id));
+  };
+  const editBtn = () => {};
+  const checkBtn = () => {
+    setTodos(
+      todos.map((item) => {
+        if (item.id === todo.id) {
+          return {
+            ...item,
+            completed: !item.completed,
+          };
         }
         return item;
-    })
+      })
     );
-};
+  };
 
-    return (
-        <div className="todo">
-            <button onClick={checkBtn} className="check">&#10004;</button>
-            <li className={`todo-item ${todo.completed ? "completed" : ""}`}>{text}</li>
-            <button onClick={editBtn} className="edit">&#9998;</button>
-            <button onClick={deleteBtn} className="delete">&#10008;</button>
-        </div>
-    );
+  return (
+    <div className="todo">
+      <button onClick={checkBtn} className="check">
+        &#10004;
+      </button>
+      <li className={`todo-item ${todo.completed ? "completed" : ""}`}>
+        {text}
+      </li>
+      <button onClick={editBtn} className="edit">
+        &#9998;
+      </button>
+      <button onClick={deleteBtn} className="delete">
+        &#10008;
+      </button>
+    </div>
+  );
 };
 
 export default Todo;
